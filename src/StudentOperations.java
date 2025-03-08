27860e9c35f4a83f70d1eeb42e6f94cf5f95e465
clown6910clown <tushar.garg.btech2023@sitpune.edu.in>
2025-03-08 15:08:04 +0530

    done with creating a student info

--- src/StudentOperations.java
+++ src/StudentOperations.java
@@ -14,6 +14,38 @@ public class StudentOperations {
                      "(PRN, Name, Branch, Batch, CGPA)" +
                      "VALUES (?,?,?,?,?)")) {
 
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter PRN ");
+            int prn = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Enter name: ");
+            String name = scanner.nextLine();
+
+            System.out.println("Enter Branch: ");
+            String branch = scanner.nextLine();
+
+            System.out.println("Enter Batch: ");
+            String batch = scanner.nextLine();
+
+            System.out.println("Enter CGPA: ");
+            float cgpa = scanner.nextFloat();
+
+            stmt.setInt(1, prn);
+            stmt.setString(2, name);
+            stmt.setString(3, branch);
+            stmt.setString(4, batch);
+            stmt.setFloat(5, cgpa);
+
+            int rowsInserted = stmt.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("Student record inserted DONE!!!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
+
+
 }
