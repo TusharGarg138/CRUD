9d9bbc9a58fbc1dd567f499d9dc0f1e09e0a3051
clown6910clown <tushar.garg.btech2023@sitpune.edu.in>
2025-03-08 15:05:40 +0530

    connection creating

--- src/StudentOperations.java
+++ src/StudentOperations.java
@@ -1,3 +1,11 @@
 import java.sql.*;
 import java.util.Scanner;
 
+public class StudentOperations {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/student_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+
+}
