63803e338f43292ab3582db461e15e611f332386
clown6910clown <tushar.garg.btech2023@sitpune.edu.in>
2025-03-08 15:10:00 +0530

    main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main{
     public static void main(String[] args){
         StudentOperations sO = new StudentOperations();
         // sO.insertStudent();
-
+        sO.viewStudent();
     }
 } // update
 // delete via prn
